6c9ae07b8ebc6516a1d764f54b87413c9bba46c5
Bel <isabelvillegas915@gmail.com>
2023-03-09 16:39:56 +0100

    Subida del repositorio al master

---
+++ forms/contact.php
@@ -0,0 +1,26 @@
+<?php
+//activar visualizacion errores
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$headers = "From: isabelvillegas915@gmail.com\r\n";
+$headers .= "Reply-To: $from\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+if(mail($email, $name, $subject, $message, $headers)){
+	echo "email correctly sent";
+} else{
+		$errorMessage = error_get_last()['message'];
+		echo $errorMessage;
+}
+
+//mail($correo_destino, $nombre, $asunto, $mensaje);
+
+?>
